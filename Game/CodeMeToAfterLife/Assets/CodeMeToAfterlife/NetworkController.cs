79f8b4a29ec9fd5edee197d43ece89709db723fb
Ivan Imbert <ivan.imbert@epita.fr>
2021-02-07 15:24:34 +0100

    Menus de base done
    Les menus de base sont fait, plus qu'à les adapter et à les mettre implémenter

---
+++ Game/CodeMeToAfterLife/Assets/CodeMeToAfterlife/NetworkController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    /*****************************************************
+     *
+     * Documentation : https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+     * Scripting API : https://doc-api.photonengine.com/en/pun/v2/index.html
+     * 
+     *****************************************************/
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Now connected to the " + PhotonNetwork.CloudRegion + " server !");
+    }
+    void Update()
+    {
+        
+    }
+}
