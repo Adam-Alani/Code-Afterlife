57fb4d54b79b518c1436f18b4a6e40c655a84f93
Ivan Imbert <ivan.imbert@epita.fr>
2021-02-25 07:37:54 +0100

    Modified the Option Menu
    Added size dropdown and full screen
    (drop down not working well yet

--- Game/CodeMeToAfterLife/Assets/Scenes/Option canvas/OptionsMenu.cs
+++ Game/CodeMeToAfterLife/Assets/Scenes/Option canvas/OptionsMenu.cs
@@ -2,12 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start ()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionList = new List<string>();
+
+        foreach (Resolution res in resolutions)
+        {
+            string option = res.width + " x " + res.height;
+            resolutionList.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(resolutionList);
+    }
+
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void SetQuality (int qualityIndex) 
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool fullscreen) 
+    {
+        Screen.fullScreen = fullscreen;
+    }
+
 }
