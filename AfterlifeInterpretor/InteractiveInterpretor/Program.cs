6747b5af50b35508fbafa55805a97e9848ceeb34
raphael.montes <raphael.montes@epita.fr>
2021-02-28 05:19:06 +0100

    Now a class library

--- AfterlifeInterpretor/InteractiveInterpretor/Program.cs
+++ AfterlifeInterpretor/InteractiveInterpretor/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AfterlifeInterpretor;
 using AfterlifeInterpretor.CodeAnalysis;
 
-namespace AfterlifeInterpretor
+namespace InteractiveInterpretor
 {
     class Program
     {
