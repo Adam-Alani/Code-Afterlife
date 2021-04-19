aae7a078d558004efcab7c121de09aab73f721a9
Théo <75431647+Seowlfh@users.noreply.github.com>
2021-04-19 11:32:55 +0200

    Movement finished
    - synchronization with multiplayer (added a "photon transform view" component to the player)

--- Game/CodeMeToAfterLife/Assets/PlayerMovement.cs
+++ Game/CodeMeToAfterLife/Assets/PlayerMovement.cs
@@ -12,46 +12,71 @@ public class PlayerMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
-    public Vector3 forward; // used to compute the vertical axis in isometric system
-    public Vector3 right; // used to compute the horizontal axis in isometric system
-
-    private PhotonView PV;
+    public Vector3 forward; // vertical axis in isometric system
+    public Vector3 right; // horizontal axis in isometric system
+    public PhotonView PV;
 
     // Start is called before the first frame update
     void Start()
     {
-        // set forward to the forward axis (blue) of the main camera
-        // usefull to not compute isometric coordinates
+        Setup();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+            Move();
+    }
+
+    /// <summary>
+    /// Setup the forward and right axis in isometric system
+    /// </summary>
+    void Setup()
+    {
         forward = Camera.main.transform.forward;
         forward.y = 0; 
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-
-        PV = GetComponent<PhotonView>();
     }
-    
-    // Update is called once per frame
-    // an optimized way is to store the previous vector in a variable, and only change motin when input changes
-    void Update()
-    {
-        // if the window is not the one with our characater, do nothing (differentiate the input)
-        if (!PV.IsMine)
-            return;
 
-        // computation of the normalized direction vector
+    /// <summary>
+    ///  Perform the Movement
+    /// </summary>
+    // an optimized way is to store the previous vector in a variable, and only change motin when input changes
+    void Move()
+    {
+        Vector3 direction = GetDirection();
+
+        if (direction.magnitude >= 0.1f)
+        {
+            PlayerRotate(direction);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Use the key inputs to compute the direction of the player
+    /// </summary>
+    Vector3 GetDirection()
+    {
         Vector3 direction = forward * Input.GetAxisRaw("Vertical") + right * Input.GetAxisRaw("Horizontal");
         direction.y = 0f;
         direction = Vector3.Normalize(direction);
 
-        if (direction.magnitude >= 0.1f)
-        {
-            // rotation of the character
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation =  Quaternion.Euler(0f, angle, 0f); // rotation around the y axis
-
-            // movement
-            controller.Move(direction * speed * Time.deltaTime);
-        }
+        return direction;
     }
+    
+    /// <summary>
+    /// Rotation the player based on his direction
+    /// </summary>
+    void PlayerRotate(Vector3 direction)
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        
+        transform.rotation =  Quaternion.Euler(0f, angle, 0f); // rotation around the y axis
+    }
+    
+    
 }
