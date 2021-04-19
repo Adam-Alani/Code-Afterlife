af6d803274c43bba629afbc48ac60b6a42f620be
Théo <75431647+Seowlfh@users.noreply.github.com>
2021-04-19 15:18:18 +0200

    TerminalBehavior
    - should be triggered on the press of a key (T)
    - Interactible class added
    - Interactible Sphere added

---
+++ Game/CodeMeToAfterLife/Assets/Scripts/Interactible.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactible : MonoBehaviour
+{
+    public float radius = 3f;
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isInRange) // if we're in range to interact
+        {
+            if (Input.GetKeyDown(interactKey)) // if the right key is pressed
+            {
+                interactAction.Invoke(); // fire event
+            }
+        }
+    }
+    
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);  
+    }
+    
+    private void OnTriggerEnter3D(SphereCollider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+    
+    private void OnTriggerExit3D(SphereCollider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player now is no longer in range");
+        }
+    }
+    
+
+}
---
+++ Game/CodeMeToAfterLife/Assets/TerminalBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalBehavior : MonoBehaviour
+{
+    public bool isOpen;
+    public Animator animator; // if we want to add an animation
+
+    public void OpenTerminal()
+    {
+        if (!isOpen)
+        {
+            // do stuff 
+            isOpen = true;
+            Debug.Log("Terminal is open");
+            animator.SetBool("IsOpen", isOpen);
+        }
+    }
+}
