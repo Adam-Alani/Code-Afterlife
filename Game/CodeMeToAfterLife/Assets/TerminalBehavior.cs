44c6d0348ffeb2b3adbbabe431b68ba61280d12c
Theo Gardet <theo.gardet@epita.fr>
2021-04-20 13:26:30 +0200

    Hopefully this commits countinas the Terminal script by adam

--- Game/CodeMeToAfterLife/Assets/TerminalBehavior.cs
+++ Game/CodeMeToAfterLife/Assets/TerminalBehavior.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TerminalBehavior : MonoBehaviour
 {
     public bool isOpen;
-    public Animator animator; // if we want to add an animation
+    // public Animator animator; // if we want to add an animation
+    
+    private Scene currentScene;
 
 	// OpenTerminal() is the function that we fire with the interactible script
     public void OpenTerminal()
@@ -13,8 +16,24 @@ public class TerminalBehavior : MonoBehaviour
         if (!isOpen)
         {
             isOpen = true;
+            currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene("Assets/CodeEditor/Code Editor.unity");
             Debug.Log("Terminal is open");
-            animator.SetBool("IsOpen", isOpen); // if we want to add an animation
+            
+            // animator.SetBool("IsOpen", isOpen); // if we want to add an animation
         }
     }
+    
+    // CloseTerminal() is called on when we close the terminal from the codeEditor
+    public void CloseTerminal()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            Debug.Log("Terminal is closed");
+            SceneManager.LoadScene(currentScene.path);
+        }
+    }
+    
+    
 }
