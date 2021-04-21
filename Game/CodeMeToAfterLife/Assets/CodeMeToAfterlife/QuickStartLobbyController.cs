0fe4a01317f551cdf3cf942a47a406c3b053a6b7
Ivan Imbert <ivan.imbert@epita.fr>
2021-04-21 11:40:30 +0200

    Private Match making system

--- Game/CodeMeToAfterLife/Assets/CodeMeToAfterlife/QuickStartLobbyController.cs
+++ Game/CodeMeToAfterLife/Assets/CodeMeToAfterlife/QuickStartLobbyController.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class QuickStartLobbyController : MonoBehaviourPunCallbacks
 {
     public GameObject quickStartButton;
     public GameObject quickCancelButton;
+    public TMP_InputField TMPInputField;
     public int roomSize;
+    public bool Private;
     
     
     /// <summary>
@@ -18,7 +21,6 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
-        quickStartButton.SetActive(true);
     }
 
     /// <summary>
@@ -27,7 +29,7 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     ///     - Enables the cancel button
     ///     - Joins a random room
     /// </summary>
-    public void QuickStart()
+    public void QuickRandomStart()
     {
         quickStartButton.SetActive(false);
         quickCancelButton.SetActive(true);
@@ -42,20 +44,72 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     /// <param name="message"> Useless parameter</param>
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("Failed to join a room");
+        Debug.Log("Failed to join a random room");
         CreateRandomRoom();
     }
 
+
+    /// <summary>
+    /// When the button is pressed :
+    ///     - Disables the start button
+    ///     - Enables the cancel button
+    ///     - Joins a private room named by the room number
+    /// </summary>
+    public void QuickPrivateStart()
+    {
+        quickStartButton.SetActive(false);
+        quickCancelButton.SetActive(true);
+        string room = "Room" + TMPInputField.text;
+        PhotonNetwork.JoinRoom(room);
+        Debug.Log("Quick Start");
+    }
+
+
+
+    /// <summary>
+    /// If joining a private room didn't work, we create the room
+    /// </summary>
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a private room");
+        CreatePrivateRoom("Room" + TMPInputField.text);
+    }
     
     /// <summary>
     /// Creates a room containing roomSize players (given via Unity as 2) in a room named from Room0 to Room9999
     /// here for random, for private, 2 or 3 little things to change (later in the project)
     /// </summary>
-    void CreateRandomRoom()
+    public void CreateRandomRoom()
     {
-        Debug.Log("Creating a new room");
+        Private = false;
+        Debug.Log("Creating a new random room");
         int randomRoomNumber = Random.Range(0, 10000);
         RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte) roomSize};
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOptions);
+        Debug.Log("Room" + randomRoomNumber);
+    }
+
+
+    /// <summary>
+    /// Creates a private room containing roomSize players (given via Unity as 2) in a room named from the parameter 'Room'
+    /// </summary>
+    public void CreatePrivateRoom(string Room)
+    {
+        Private = true;
+        Debug.Log("Creating a new private room");
+        RoomOptions roomOptions = new RoomOptions() { IsVisible = false, IsOpen = true, MaxPlayers = (byte) roomSize};
+        PhotonNetwork.CreateRoom(Room, roomOptions);
+        Debug.Log(Room);
+    }
+    /// <summary>
+    /// Creates a private room containing roomSize players (given via Unity as 2) in a room named from Room0 to Room9999
+    /// </summary>
+    public void CreatePrivateRoom()
+    {
+        int randomRoomNumber = Random.Range(0, 10000);
+        Private = true;
+        Debug.Log("Creating a new private room " + randomRoomNumber);
+        RoomOptions roomOptions = new RoomOptions() { IsVisible = false, IsOpen = true, MaxPlayers = (byte) roomSize};
         PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOptions);
         Debug.Log("Room" + randomRoomNumber);
     }
@@ -69,7 +123,10 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.Log("Failed to create room, trying again");
-        CreateRandomRoom();
+        if (Private)
+            CreatePrivateRoom();
+        else  
+            CreateRandomRoom();
     }
 
     
