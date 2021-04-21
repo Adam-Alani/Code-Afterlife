a8d5cf0c4365ad9ebb49e764ae17ca7b46778251
Ivan Imbert <ivan.imbert@epita.fr>
2021-04-21 09:47:02 +0200

    HOPE THIS WORKS NOW
    !!!!!!!!!!!!!! changes are changes KEEP THEM S** ** * ***** (nice little github)

---
+++ Game/CodeMeToAfterLife/Assets/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    
+    // those variables are used to perform a smooth rotation of the player
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public Vector3 forward; // vertical axis in isometric system
+    public Vector3 right; // horizontal axis in isometric system
+    public PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+            Move();
+    }
+
+    /// <summary>
+    /// Setup the forward and right axis in isometric system
+    /// </summary>
+    void Setup()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0; 
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    /// <summary>
+    ///  Perform the Movement
+    /// </summary>
+    // an optimized way is to store the previous vector in a variable, and only change motin when input changes
+    void Move()
+    {
+        Vector3 direction = GetDirection();
+
+        if (direction.magnitude >= 0.1f)
+        {
+            PlayerRotate(direction);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Use the key inputs to compute the direction of the player
+    /// </summary>
+    Vector3 GetDirection()
+    {
+        Vector3 direction = forward * Input.GetAxisRaw("Vertical") + right * Input.GetAxisRaw("Horizontal");
+        direction.y = 0f;
+        direction = Vector3.Normalize(direction);
+
+        return direction;
+    }
+    
+    /// <summary>
+    /// Rotation the player based on his direction
+    /// </summary>
+    void PlayerRotate(Vector3 direction)
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        
+        transform.rotation =  Quaternion.Euler(0f, angle, 0f); // rotation around the y axis
+    }
+    
+    
+}
---
+++ Game/CodeMeToAfterLife/Assets/Scenes/any scene/ShowRoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class ShowRoom : MonoBehaviour
+{
+    public Text txt;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+}
