b8c9c777f3aed6ee67733c72521cdd4818c45fd4
Ivan Imbert <ivan.imbert@epita.fr>
2021-04-26 20:17:56 +0200

    Continued stuff with Tadam

---
+++ Game/CodeMeToAfterLife/Assets/CodeEditor/Puzzle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AfterlifeInterpretor;
+using System;
+public class Puzzle : MonoBehaviour
+{
+
+    public List<Func<Interpretor, (bool, string)>> Functions = new List<Func<Interpretor, (bool, string)>>();
+
+
+    void Start()
+    {
+        Functions.Add(Fibo);
+        Functions.Add(Multiply);
+    }
+
+
+    public (bool, string) Evaluate(Interpretor interpretor, int index)
+    {
+        return Functions[index](interpretor);
+    }
+
+    public (bool, string) Fibo(Interpretor interpretor)
+    {
+        string er = interpretor.Interpret("fibo 42").ToString();
+        return (er == "267914296", er); 
+    }
+
+    public (bool, string) Multiply(Interpretor interpretor)
+    {
+        string er = interpretor.Interpret("multiply 4, 2").ToString();
+        return (er == "8", er); 
+    }
+}
