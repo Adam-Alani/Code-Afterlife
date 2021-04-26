7e919d1625857b98158d11854743d94491ba3763
Ivan Imbert <ivan.imbert@epita.fr>
2021-04-26 09:02:53 +0200

    Fixed Assets/

---
+++ Game/CodeMeToAfterLife/Assets/SoundDesign/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+   public Sound[] sounds;
+   public static AudioManager instance;
+   
+   void Awake()
+   {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+       
+        DontDestroyOnLoad(gameObject);
+        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+       
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+   
+   }
+   
+   void Start ()
+   {
+	    Play("MainMusic");
+   }
+   
+   public void Play (string name)
+   {
+   		Sound s = Array.Find(sounds, sound => sound.name == name);
+   		if (s == null)
+   		{
+   			Debug.LogWarning("Sound: " + name + " not found!");
+   			return;
+   		}
+   		s.source.Play();
+   }
+}
