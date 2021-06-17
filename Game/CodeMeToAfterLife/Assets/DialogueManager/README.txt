22c20b1bf6ba4c4810b6b7c59e85e68b7e0533a3
Théo Gardet <theo.gardet@epita.fr>
2021-06-17 17:59:31 +0200

    Dialogue working !
    - dialogue Manager
    - animation
    - UI
    - sign in tutorial_map that can launch dialogue open pressing a button

---
+++ Game/CodeMeToAfterLife/Assets/DialogueManager/README.txt
@@ -0,0 +1,3 @@
+HOW TO IMPLEMENT DIALOGUE IN A SCENE:
+ * drag and drop the DialogueTrigger on the object that will trigger the dialogue (collider, button...)
+ * in Unity, in the DialogueTrigger script (on the object), select the number of sentences, the name, and write the sentences
---
+++ Game/CodeMeToAfterLife/Assets/OpenDialogue.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDialogue : MonoBehaviour
+{
+    public KeyCode openKey;
+	public Dialogue dialogue;
+
+	public bool isInRange;
+	public bool isOpen;
+
+    void Update()
+    {
+        if (isInRange) // if we're in range to interact
+        {
+            if (Input.GetKeyDown(openKey)) // if the right key is pressed
+            {
+                if (!isOpen)
+                {
+                    isOpen = true;
+                    FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+                }
+            }
+        }
+    }
+    
+    // method called by unity when there is a collision
+    // this methods requires a collider on each object that should interact, with one that has the option "is trigger" enabled, and at minimum one rigidbody on either of the objet
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+    
+    // method called by unity when the collision ends
+    // this methods requires a collider on each object that should interact, with one that has the option "is trigger" enabled, and at minimum one rigidbody on either of the objet
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+}
